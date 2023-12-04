062826416a6f17dabaf273d3a2e4e5b89cd4a7f9
Muhammad Bilal <bafridi683@gmail.com>
2023-12-04 19:07:54 +0500

    Calculator UI

---
+++ lib/components/mybutton.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget {
+  final String title;
+  final Color color;
+  final VoidCallback onPress;
+  const MyButton({
+    super.key,
+    required this.title,
+    this.color = const Color(0xffa5a5a5),
+    required this.onPress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 10),
+        child: GestureDetector(
+          onTap: onPress,
+          child: Container(
+            height: 80,
+            decoration: BoxDecoration(
+              color: color,
+              shape: BoxShape.circle,
+            ),
+            child: Center(
+              child: Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/view/home_view.dart
@@ -0,0 +1,240 @@
+import 'package:calculator/components/mybutton.dart';
+import 'package:flutter/material.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  var userInput = '';
+  var answer = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 10, horizontal: 10),
+                  child: Align(
+                    alignment: Alignment.bottomRight ,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text(
+                          userInput.toString(),
+                          style: const TextStyle(
+                              fontSize: 30, color: Colors.white),
+                        ),
+                        Text(
+                          answer.toString(),
+                          style: const TextStyle(
+                              fontSize: 30, color: Colors.white),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        MyButton(
+                          title: 'AC',
+                          onPress: () {
+                            userInput = '';
+                            answer = '';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '+/-',
+                          onPress: () {
+                            userInput += '+/-';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '%',
+                          onPress: () {
+                            userInput += '%';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '/',
+                          color: const Color(0xffffa00a),
+                          onPress: () {
+                            userInput += '/';
+                            setState(() {});
+                          },
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        MyButton(
+                          title: '7',
+                          onPress: () {
+                            userInput += '7';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '8',
+                          onPress: () {
+                            userInput += '8';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '0',
+                          onPress: () {
+                            userInput += '0';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: 'x',
+                          color: const Color(0xffffa00a),
+                          onPress: () {
+                            userInput += 'x';
+                            setState(() {});
+                          },
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        MyButton(
+                          title: '4',
+                          onPress: () {
+                            userInput += '4';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '5',
+                          onPress: () {
+                            userInput += '5';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '6',
+                          onPress: () {
+                            userInput += '6';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '-',
+                          color: const Color(0xffffa00a),
+                          onPress: () {
+                            userInput += '-';
+                            setState(() {});
+                          },
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        MyButton(
+                          title: '1',
+                          onPress: () {
+                            userInput += '1';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '2',
+                          onPress: () {
+                            userInput += '2';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '3',
+                          onPress: () {
+                            userInput += '3';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '+',
+                          color: const Color(0xffffa00a),
+                          onPress: () {
+                            userInput += '+';
+                            setState(() {});
+                          },
+                        ),
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        MyButton(
+                          title: '0',
+                          onPress: () {
+                            userInput += '0';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '.',
+                          onPress: () {
+                            userInput += '.';
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: 'DEL',
+                          onPress: () {
+                            userInput =
+                                userInput.substring(0, userInput.length - 1);
+                            setState(() {});
+                          },
+                        ),
+                        MyButton(
+                          title: '=',
+                          color: const Color(0xffffa00a),
+                          onPress: () {
+                            equalPress();
+                            setState(() {});
+                          },
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void equalPress() {
+    String finalUserInput = userInput.replaceAll('x', '*');
+    Parser p = Parser();
+    Expression expression = p.parse(finalUserInput);
+    ContextModel contextModel = ContextModel();
+
+    double eval = expression.evaluate(EvaluationType.REAL, contextModel);
+    answer = eval.toString();
+  }
+}
